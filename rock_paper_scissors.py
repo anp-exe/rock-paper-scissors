17df995f24cb6c6fa3a4ffd32f171e90cdcdfa0a
Anna Parker <xannxbelle05@icloud.com>
2026-04-15 21:13:14 +0100

    Refactor tie condition logic in Rock Paper Scissors game to remove redundancy

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,7 +7,7 @@ human = int(input("Pick a number: "))
 print('Computer picked: ', computer)
 print('You picked: ', human, '\n')
 
-if computer == 1 and human == 1:
+if computer == human:
     print("It's a tie!")
 elif computer == 2 and human == 1:
     print('You Lost!')
@@ -15,15 +15,11 @@ elif computer == 3 and human == 1:
     print('You Won!')
 elif computer == 1 and human == 2:
     print('You Won!')
-elif computer == 2 and human == 2:
-    print('It\'s a tie!')
 elif computer == 3 and human == 2:
     print('You Lost!')
 elif computer == 1 and human == 3:
     print('You Lost!')
 elif computer == 2 and human == 3:
     print('You Won!')
-elif computer == 3 and human == 3:
-    print("It's a tie!")
 else:
     print('Please input a number between 1 and 3!')
